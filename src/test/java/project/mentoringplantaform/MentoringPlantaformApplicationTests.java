7d5f7e21cab88d6c3b9a7a68b203a3fcff6058d7
Anthonnytauchen <162629790+Anthonnytauchen@users.noreply.github.com>
2026-01-30 19:29:03 -0300

    My firsh commit

---
+++ src/test/java/project/mentoringplantaform/MentoringPlantaformApplicationTests.java
@@ -0,0 +1,13 @@
+package project.mentoringplantaform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringPlantaformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
